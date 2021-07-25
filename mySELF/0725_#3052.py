faf235dd950ad6ffb16995a0805132c65b2172e8
mathtkang <ksge1124@gmail.com>
2021-07-25 18:26:33 +0900

    0725

---
+++ mySELF/0725_#3052.py
@@ -0,0 +1,17 @@
+# 나머지
+import sys
+input = sys.stdin.readline
+
+# 나머지 담는 리스트
+num_list = []
+
+for i in range(10):
+    num = int(input()) % 42
+    num_list.append(num)
+
+# num_list에서 중복 제거
+# => 데이터 타입 : set->list
+num_set = set(num_list)
+num_list = list(num_set)
+
+print(len(num_list))
