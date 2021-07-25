045e4466475fa9dd51ddedbfb89ec15081a81124
mathtkang <ksge1124@gmail.com>
2021-07-25 17:28:43 +0900

    0725

--- [Egg to kkokko] Baek/baek5_#11399.py
+++ [Egg to kkokko] Baek/baek5_#11399.py
@@ -1,1 +1,15 @@
-# https://www.acmicpc.net/problem/11399
+# ATM
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = list(map(int, input().split()))
+
+p.sort()
+sum = 0
+
+for i in range(n):
+    for j in range(i+1):
+        sum += p[j]
+
+print(sum)
