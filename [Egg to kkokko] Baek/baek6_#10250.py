cb9f5522167f20608522e95dd13c137384171a59
mathtkang <ksge1124@gmail.com>
2021-07-27 20:08:18 +0900

    0727

--- [Egg to kkokko] Baek/baek6_#10250.py
+++ [Egg to kkokko] Baek/baek6_#10250.py
@@ -6,10 +6,16 @@ lst = []
 
 for _ in range(people):
     H, W, N = map(int, input().split())
-    Q = (N//H)+1  # 몫
-    R = N % H  # 나머지
-    room_num = R*100 + Q
-    lst.append(room_num)
+    Q = N//H  # 몫 -> 호
+    R = N % H  # 나머지 -> 층
+
+    if R == 0:
+        Q = N//H
+        R = H  # 맨 꼭대기 층 수
+    else:
+        Q = N//H+1
+        R = N % H
+    lst.append(R*100 + Q)
 
 for i in range(len(lst)):
     print(lst[i])
