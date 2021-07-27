300c476a35065950c517805327546577161f557e
mathtkang <ksge1124@gmail.com>
2021-07-27 19:52:00 +0900

    0727

--- [Egg to kkokko] Baek/baek6_#10250.py
+++ [Egg to kkokko] Baek/baek6_#10250.py
@@ -1,2 +1,15 @@
 # 10250 : ACM호텔
 # https://www.acmicpc.net/problem/10250
+
+people = int(input())
+lst = []
+
+for _ in range(people):
+    H, W, N = map(int, input().split())
+    Q = (N//H)+1  # 몫
+    R = N % H  # 나머지
+    room_num = R*100 + Q
+    lst.append(room_num)
+
+for i in range(len(lst)):
+    print(lst[i])
