4b988d4b2b4bafa5bf4bf5a0bb5a7282cfebb809
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-17 23:46:58 +0900

    0917

--- elice/elice_step2_#math10.py
+++ elice/elice_step2_#math10.py
@@ -1,4 +1,25 @@
-# [수학]
+# [수학] 근거 없는 자신감
+'''
+https://velog.io/@aonee/k-s-listmapintinput.split
 '''
 
-'''
+import sys
+input = sys.stdin.readline
+
+N, *lst = list(map(int, input().split()))
+# print(lst)
+
+result = sum(lst)/N
+# print(result)
+
+
+# 평균을 넘는 학생 비율 (평균값 포함 안됨) 초과!!
+p = 0
+
+for i in range(N):
+    if lst[i] > result:
+        p += 1
+
+processpoint = p*100/N
+
+print("%0.3f%%" % processpoint)
--- elice/elice_step2_#math9.py
+++ elice/elice_step2_#math9.py
@@ -1,4 +1,11 @@
-# [수학]
+# [수학] 버스
 '''
+최소공배수 구하는 문제
+'''
+import math
+import sys
+input = sys.stdin.readline
 
-'''
+s, t = map(int, input().split())
+
+print(math.lcm(s, t))
