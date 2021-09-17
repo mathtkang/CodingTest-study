fe70bbe675715d89a62f2400d8d9c7c52a037aaa
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-17 23:32:06 +0900

    0917

--- elice/elice_step2_#greedy1.py
+++ elice/elice_step2_#greedy1.py
@@ -8,3 +8,11 @@ C, S, E = map(int, input().split())
 if C//2 >= S:
     result = S
     remain = C % 2 + (C//2 - S)
+else:
+    result = C//2
+    remain = C % 2 + (S - C//2)
+
+if remain >= E:
+    print(result)
+else:
+    print(result + (remain-E)//3)
--- elice/elice_step2_#greedy2.py
+++ elice/elice_step2_#greedy2.py
@@ -2,3 +2,13 @@
 '''
 
 '''
+N = int(input())
+exchange = 10000-N
+
+result = 0
+for i in range(4, -1, -1):
+    val = exchange // pow(10, i)
+    result += val
+    exchange -= val*pow(10, i)
+
+print(result)
