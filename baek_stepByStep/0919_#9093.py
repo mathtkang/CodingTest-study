ad3cc6aba3269caf79d14ccb24bd0cef5a5e62a9
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-20 04:05:08 +0900

    0920

---
+++ baek_stepByStep/0919_#9093.py
@@ -0,0 +1,18 @@
+# 단어 뒤집기
+'''
+첫째줄 : 앞으로 몇개의 list가 입력될지
+둘째줄~ : 한 줄이 하나의 리스트가 되도록
+리스트 생성 후, 각 요소들을 [::-1]로 뒤집어준다. 이후 바로 출력
+'''
+
+N = int(input())
+printf = ''
+
+for i in range(N):
+    lst = list(map(str, input().split(" ")))
+    for j in range(len(lst)):
+        string = lst[j]
+        lst[j] = string[::-1]
+        printf += lst[j] + ' '
+    printf += '\n'
+print(printf)
--- elice_step2/elice_step2_#math3.py
+++ elice_step2/elice_step2_#math3.py
@@ -4,43 +4,51 @@
 앞면을 0, 뒷면을 1이라고 하자
 길이가 N인 리스트를 만들고 안의 값을 0(앞면이 보이도록)으로 초기화 한다
 
-매개변수(p)의 배수에 해당하는 index를 list(index)로 호출, 이때 list(index)의 값이 0이면 1로, 1이면 0으로 바꾸는 함수를 만든다
-for문을 통해서 1~N까지 함수를 호출하고 최종적인 list에서 뒷면(1)의 갯수를 카운트
+1부터 N까지 각각 배수에 해당하는 index를 list(index)로 호출, 이때 list(index)의 값이 0이면 1로, 1이면 0으로 바꾸는 로직을 만든다
+
+=> 이렇게 하면 시간초과 발생
+
+[또 다른 접근]
+0에서 1로, 1에서 0으로 여러번 바꾸는게 아니라 어떤 수가 바뀌는지 찾고 해당하는 숫자만 count해준다
+
+기존의 카드는 앞면(0)-> 이를 홀수번 뒤집어야 뒷면(1)이 된다.
+
+(자신의 약수가 앞에서 존재하면 자신을 바꾸기 때문에 바뀌는 횟수가 홀수가 되기위해서는 약수의 갯수가 홀수여야 한다.)
+
+약수의 갯수가 홀수인 수는 완전제곱수이므로,
+따라서, 1부터 N까지 제곱수의 갯수 = 뒷면이 보이게되는 카드의 수
 '''
-
 N = int(input())
-lst = [0 for i in range(N)]  # 길이가 N, 각각의 값이 0으로 초기화 된 리스트
-lst_idx = []  # 값을 바꿔줄 인덱스 담는 리스트
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        idx = i*j
-        if idx <= N:
-            lst_idx.append(idx)
+# sol1) 시간초과 발생
+# lst = [0 for i in range(N)]  # 길이가 N, 각각의 값이 0으로 초기화 된 리스트
+# lst_idx = []  # 값을 바꿔줄 인덱스 담는 리스트
 
-# print(lst_idx)
+# for i in range(1, N+1):
+#     for j in range(1, N+1):
+#         idx = i*j
+#         if idx <= N:
+#             lst_idx.append(idx)
 
-# 인덱스에 따라서 값을 바꿔주자 (0<->1)
-for i in lst_idx:
-    if lst[i-1] == 0:
-        lst[i-1] = 1
-    elif lst[i-1] == 1:
-        lst[i-1] = 0
+# # print(lst_idx)
 
-# print(lst)
+# # 인덱스에 따라서 값을 바꿔주자 (0<->1)
+# for i in lst_idx:
+#     if lst[i-1] == 0:
+#         lst[i-1] = 1
+#     elif lst[i-1] == 1:
+#         lst[i-1] = 0
 
-print(lst.count(1))
+# # print(lst)
+
+# print(lst.count(1))
 
 
-# for i in range(N):
-#     change(i+1)
+# sol2) 또 다른 접근방식
+lst_ = []
+for i in range(1, N//2+1):
+    pow_ = i*i
+    if pow_ <= N:
+        lst_.append(pow_)
 
-# def change(매개변수):
-#     for i in range(1, N):
-#         idx = p*(i+1)
-#         if lst[idx] == 0:
-#             lst[idx] = 1
-#         elif lst[idx] == 1:
-#             lst[idx] = 0
-#     # if
-#     pass
+print(len(lst_))
