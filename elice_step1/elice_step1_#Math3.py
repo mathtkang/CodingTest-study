ecdcf8efa737f67b8f49bedb836540dd1c45114c
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-19 13:59:53 +0900

    0919

--- elice_step1/elice_step1_#Math3.py
+++ elice_step1/elice_step1_#Math3.py
@@ -3,7 +3,7 @@
 [문제 접근 방식]
 
 N으로 숫자를 받고
-num = math.pow(2,N)
+num = math.pow(2,N) -> 여기서는 math.pow(는 float형)가 아니라 내장함수 pow를 사용
 num을 각각 한자리 씩 더해준다
 '''
 import sys
--- elice_step1/elice_step1_#Math4.py
+++ elice_step1/elice_step1_#Math4.py
@@ -2,33 +2,16 @@
 '''
 [문제 접근 방식]
 초로 이루어진 모든 시간을 다 더한 후,
-분,초로 변환해준다
+분,초 로 변환해준다
 '''
-# global s
-# s = int(input("초를 입력하시오."))
-
-# def hms():
-#     global s
-#     hours = s // 3600
-#     s = s - hours*3600
-#     mu = s // 60
-#     ss = s - mu*60
-#     print(hours, '시간', mu, '분', ss, '초 입니다.')
-
-# hms()
-
-# m, s = divmod(seconds, 60)
-# h, m = divmod(m, 60)
-
-
-from datetime import timedelta
-lst = []
+sum_ = 0
 for _ in range(4):
-    num = int(input())
-    lst.append(num)
+    sum_ += int(input())
 
-sum_seconds = 0
-for i in range(len(lst)):
-    sum_seconds += lst[i]
+# print(sum_)
 
-print(datetime.timedelta(seconds=sum_seconds))
+x = sum_//60
+y = sum_ % 60
+
+print(x)
+print(y)
