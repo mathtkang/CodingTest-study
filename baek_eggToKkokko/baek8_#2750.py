0841555497a5bae0437a0d2b23676f9af5186989
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-21 23:59:44 +0900

    0921

---
+++ baek_eggToKkokko/baek8_#2750.py
@@ -0,0 +1,18 @@
+# 수 정렬하기
+# https://www.acmicpc.net/problem/2750
+
+N = int(input())
+
+numbers = []
+
+for _ in range(N):
+    numbers.append(int(input()))
+
+# Bubble Sort
+for i in range(len(numbers)):
+    for j in range(len(numbers)):
+        if numbers[i] < numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+for n in numbers:
+    print(n)
