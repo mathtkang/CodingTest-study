4c9ad9f61a505a7c8df1fd741f6df4df22ee8093
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-23 18:55:08 +0900

    0923

--- elice_step2/elice_step2_#구현.py
+++ elice_step2/elice_step2_#구현.py
@@ -1,3 +1,14 @@
 # [구현] 직각삼각형
 '''
+https://www.acmicpc.net/problem/4153
 '''
+while True:
+    a = list(map(int, input().split()))
+    max_num = max(a)
+    if sum(a) == 0:
+        break
+    a.remove(max_num)
+    if a[0] ** 2 + a[1] ** 2 == max_num ** 2:
+        print('rightangle')
+    else:
+        print('triangle')
