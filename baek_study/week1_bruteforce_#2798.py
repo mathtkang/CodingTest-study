a2c754e412f50e3b2aea9287f40504fe34f166a3
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-09-28 01:50:52 +0900

    0928

--- baek_study/week1_bruteforce_#2798.py
+++ baek_study/week1_bruteforce_#2798.py
@@ -1,4 +1,24 @@
 # 블랙잭
 # https://www.acmicpc.net/problem/2798
 '''
+[문제 접근 방식]
+N개중 3개를 순서없이 뽑고, 그 3개의 값의 합들 중 M을 넘지않는 최대값을 반환
 '''
+
+from itertools import combinations
+import sys
+sys.stdin = open("input.txt")
+
+N, M = map(int, input().split())
+card_list = list(map(int, input().split()))
+
+list_combi = list(combinations(card_list, 3))
+
+result = []
+
+for i in list_combi:
+    combi_sum = sum(i)
+    if combi_sum <= M:
+        result.append(combi_sum)
+
+print(max(result))
