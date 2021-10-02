94c582187514fefe7e462d2772c07f1dad1e6668
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-02 23:56:09 +0900

    1002

--- baek_study/week2_#13305_greedy.py
+++ baek_study/week2_#13305_greedy.py
@@ -5,3 +5,15 @@
 
 [문제 접근 방식]
 '''
+N = int(input())
+roads = list(map(int, input().split()))
+cities = list(map(int, input().split()))
+
+minVal = cities[0]
+sum = 0
+for i in range(N-1):
+    if minVal > cities[i]:
+        minVal = cities[i]
+    sum += (minVal * roads[i])
+
+print(sum)
