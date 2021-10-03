a45619f673e0e630ec099435b8266e059bbc161d
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-03 16:51:12 +0900

    1003

--- baek_study/week2_#13305_greedy.py
+++ baek_study/week2_#13305_greedy.py
@@ -2,18 +2,56 @@
 # https://www.acmicpc.net/problem/13305
 '''
 [문제 이해]
+첫째줄 : 마을의 갯수
+둘째줄 : 각 마을 사이의 도로의 갯수 => 내가 채워야하는 리터(양)
+셋째줄 : 마을에서 채울 수 있는 기름의 1리터 당 가격
+처음에는 기름이 없음 -> 첫번째 마을에서는 무조건 결제해야 함
+마지막 마을의 가격은 상관없음
 
 [문제 접근 방식]
+L과 W의 리스트 갯수는 W가 하나 더 많다
+마지막 마을의 가격은 필요 없으니까 W의 마지막 원소를 제거
+L과 W의 순서를 바꿔준다.
+
+셋째줄의 마을에서 채울 수 있는 기름의 리터들을 비교해서 
 '''
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
 N = int(input())
-roads = list(map(int, input().split()))
-cities = list(map(int, input().split()))
+L = list(map(int, input().split()))
+W = list(map(int, input().split()))
+# 마지막 원소 제거
+W.pop()
+# 뒤집어서 정렬
+L.reverse()
+W.reverse()
 
-minVal = cities[0]
-sum = 0
-for i in range(N-1):
-    if minVal > cities[i]:
-        minVal = cities[i]
-    sum += (minVal * roads[i])
+# print(N)
+# print(L)
+# print(W)
 
-print(sum)
+# 더 간단하게는?
+re_L = []
+for i in range(len(L)):
+    sum_ = 0
+    for j in range(i+1):
+        sum_ += L[j]
+    re_L.append(sum_)
+
+print(re_L)  # [1, 4, 6]
+
+# for i in L:
+#     sum_ += i
+#     L[i:]
+
+# 타입에러 : i+1가 존재하지 않음 (그렇다고 i=1부터 하면 전체가 계수되지 않아서 값이 달라짐)
+result = 0
+for i in range(len(W)):
+    if W[i] > W[i+1]:  # 4 > 2
+        result += W[i+1]*re_L[i+1]
+        if i+1 == len(W):
+            break
+    result += W[i]*L[i]
+
+print(result)
