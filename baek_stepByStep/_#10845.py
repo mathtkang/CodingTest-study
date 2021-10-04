009cb27ee1d9ced77db05f8deef357fd51b83ab4
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-05 01:48:48 +0900

    1005

---
+++ baek_stepByStep/_#10845.py
@@ -0,0 +1,15 @@
+# 큐
+# https://www.acmicpc.net/problem/10845
+'''
+[문제 접근 방식]
+'''
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+num = map(int, input().split)
+
+queue = []
+
+for i in range(num):
+    pass
