95b9384cdaee3ff286b1dace1b49f0a5d78adbfd
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-05 01:03:29 +0900

    1004

--- baek_study/week2_#11047_greedy.py
+++ baek_study/week2_#11047_greedy.py
@@ -1,7 +1,22 @@
 # 동전 0
 # https://www.acmicpc.net/problem/11047
-'''
-[문제 이해]
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
 
-[문제 접근 방식]
-'''
+N, K = map(int, input().split())
+lst = []
+for _ in range(N):
+    lst.append(int(input()))
+
+cnt = 0
+for i in lst[::-1]:
+    if K == 0:
+        break
+    if i > K:
+        continue
+    else:
+        cnt += K//i  # 현재 단위(i)의 몫
+        K %= i  # 현재 단위(i)의 나머지
+
+print(cnt)
