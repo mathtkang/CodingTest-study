41e69fe892622eee6891fcf02650e44ea40f206f
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-05 01:49:08 +0900

    1005

--- baek_stepByStep/0718_#10818.py
+++ baek_stepByStep/0718_#10818.py
@@ -1,4 +1,5 @@
-# 10818 : 1차원 배열 > 최소,최대
+# 10818 : 1차원 배열 > 최소,최대 (성공)
+# https://www.acmicpc.net/problem/10818
 
 # 입력받을 개수 정하기 -> 리스트의 개수 정하기
 # 입력받은 것들 중에서 최소와 최대를 각각 출력
@@ -8,12 +9,12 @@ input = sys.stdin.readline
 num = int(input())
 num_lst = list(map(int, input().split()))
 
-# python 내장 함수 사용
+# sol1) python 내장 함수 사용
 print(min(num_lst))
 print(max(num_lst))
 print(min(num_lst), max(num_lst), end='')
 
-# 직접 구현하기
+# sol2) 직접 구현하기
 min = num_lst[0]
 max = num_lst[0]
 for i in range(num):
