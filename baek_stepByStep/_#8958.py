e5d206642d6e7f0f66d27b1a053662518011c787
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-09 23:59:23 +0900

    1009

--- baek_stepByStep/_#8958.py
+++ baek_stepByStep/_#8958.py
@@ -3,3 +3,16 @@
 '''
 [문제 접근 방식]
 '''
+a = int(input())
+for i in range(a):
+    b = input()
+    s = list(b)
+    sum = 0
+    c = 1
+    for i in s:
+        if i == 'O':
+            sum += c
+            c += 1
+        else:
+            c = 1
+    print(sum)
