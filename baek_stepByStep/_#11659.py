fbe909a8afbcb8e8dc6372e2585ff60156a0f6ef
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-10 23:43:20 +0900

    1010

--- baek_stepByStep/_#11659.py
+++ baek_stepByStep/_#11659.py
@@ -3,3 +3,21 @@
 '''
 [문제 접근 방식]
 '''
+import sys
+
+N, M = map(int, input().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+sum_numbers = [0 for _ in range(N)]
+
+for i in range(N):
+    if i == 0:
+        sum_numbers[i] = numbers[i]
+    else:
+        sum_numbers[i] = sum_numbers[i-1] + numbers[i]
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    if i == 1:
+        print(sum_numbers[j-1])
+    else:
+        print(sum_numbers[j-1] - sum_numbers[i-2])
