db1e68cf325192e45c2dbecca925bd5a355d4a72
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-12 22:26:01 +0900

    1012

--- baek_study/week3_#2751_sort.py
+++ baek_study/week3_#2751_sort.py
@@ -4,3 +4,16 @@
 [문제 이해]
 [문제 접근 방식]
 '''
+# pypy3 이용
+n = int(input())
+num = []
+
+for _ in range(n):
+    x = int(input())
+    num.append(x)
+
+for i in sorted(num):
+    print(i)
+
+#
+# https://assaeunji.github.io/python/2020-05-06-bj2751/
