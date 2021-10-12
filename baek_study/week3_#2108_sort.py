787da13c05f2a09e1027e51b40a9edb3b58af605
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-12 22:25:54 +0900

    1012

--- baek_study/week3_#2108_sort.py
+++ baek_study/week3_#2108_sort.py
@@ -4,3 +4,20 @@
 [문제 이해]
 [문제 접근 방식]
 '''
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for _ in range(N):
+    lst.append(int(input()))
+
+# 산술평균
+print()
+
+# 중앙값
+
+# 최빈값
+
+# 범위(최댓값-최솟값)
