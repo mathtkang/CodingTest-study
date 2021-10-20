0c46ee0e995dde27ff2a54d924d2c2ad65e613ee
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-10-20 19:42:22 +0900

    1020

--- baek_study/week4_#10773.py
+++ baek_study/week4_#10773.py
@@ -1,6 +1,20 @@
-# 제로
+# 제로 (성공)
 # https://www.acmicpc.net/problem/10773
 '''
 [문제 이해]
 [문제 접근 방식]
 '''
+import sys
+sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+K = int(input())
+
+stack = []
+for _ in range(K):
+    num = int(input())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+print(sum(stack))
