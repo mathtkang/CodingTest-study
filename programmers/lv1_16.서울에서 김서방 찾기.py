2140dfd349958b55caf25c8272e5719a470fb184
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-11-03 23:51:52 +0900

    1103

---
+++ programmers/lv1_16.서울에서 김서방 찾기.py
@@ -0,0 +1,6 @@
+def solution(seoul):
+    for index, name in enumerate(seoul):
+        if name == "Kim":
+            answer = "김서방은 "+str(index)+"에 있다"  # index는 int형이므로 str형태로 변환해주어야함
+
+    return answer
