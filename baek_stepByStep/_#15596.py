51e83480d0a15eebcbf7bdf01a982bd9c23200c4
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-11-09 23:57:58 +0900

    1109

--- baek_stepByStep/_#15596.py
+++ baek_stepByStep/_#15596.py
@@ -2,4 +2,15 @@
 # https://www.acmicpc.net/problem/15596
 '''
 [문제 접근 방식]
+
 '''
+
+
+def solve(a):
+    # sol1) sum함수 이용
+    return sum(a)
+    # sol2) for문 이용
+    # result = 0
+    # for i in a:
+    #     result += i
+    # return result
