4f4a2b8cbd56d20e6e3c63a49bb4bc30d839e376
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-11-13 23:39:51 +0900

    1113

--- baek_stepByStep/_#19532.py
+++ baek_stepByStep/_#19532.py
@@ -3,3 +3,12 @@
 '''
 [문제 접근 방식]
 '''
+import sys
+input = sys.stdin.readline
+
+a, b, c, d, e, f = map(int, input().split())
+
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if a*i + b*j == c and d*i + e*j == f:
+            print(i, j)
